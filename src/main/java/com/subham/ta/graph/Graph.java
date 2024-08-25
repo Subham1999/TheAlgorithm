12edcb45f86fb58c3e7ebe4f4ce936b3549ba736
Subham1999 <subhamsantra2016@gmail.com>
2024-08-25 17:42:50 +0530

    reformatting

---
+++ src/main/java/com/subham/ta/graph/Graph.java
@@ -0,0 +1,35 @@
+package com.subham.ta.graph;
+
+import java.util.HashSet;
+import java.util.Set;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Graph<K, V> {
+  private Set<Vertex<K, V>> vertexSet;
+  private Set<Edge<K, V>> edgeSet;
+  private boolean isDirected;
+
+  public Graph(boolean isDirected) {
+    this.isDirected = isDirected;
+    this.vertexSet = new HashSet<>();
+    this.edgeSet = new HashSet<>();
+  }
+
+  public void addEdge(Edge<K, V> edge) {
+    if (!edgeSet.contains(edge)) {
+      vertexSet.add(edge.getVertex1());
+      vertexSet.add(edge.getVertex2());
+      edgeSet.add(edge);
+
+      edge.getVertex1().getEdges().add(edge);
+      edge.getVertex1().getNeighbors().add(edge.getVertex2());
+      if (!this.isDirected()) {
+        edge.getVertex2().getEdges().add(edge);
+        edge.getVertex2().getNeighbors().add(edge.getVertex1());
+      }
+    }
+  }
+}
