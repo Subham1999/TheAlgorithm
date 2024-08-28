119ce56a4795e0ab4b4e1d24bc4a99caeb7d0472
Subham1999 <subhamsantra2016@gmail.com>
2024-08-28 23:50:32 +0530

    improved the algorithm

--- src/main/java/com/thealgorithm/backtracking/PalindromePartitioning.java
+++ src/main/java/com/thealgorithm/backtracking/PalindromePartitioning.java
@@ -8,46 +8,40 @@ import java.util.List;
  */
 public class PalindromePartitioning {
 
+  Boolean[][] memo;
+
   public List<List<String>> partition(String s) {
+    memo = new Boolean[s.length()][s.length()];
     List<List<String>> possiblePalindromes = new ArrayList<>();
-
-    partition(s, 0, s.length() - 1, new ArrayList<>(), possiblePalindromes);
-
+    partition(s, 0, new ArrayList<>(), possiblePalindromes);
     return possiblePalindromes;
   }
 
-  void partition(
-      String s, int lo, int hi, List<String> current, List<List<String>> possiblePalindromes) {
-    // System.out.println(current);
-    if (lo > hi) {
+  void partition(String s, int lo, List<String> current, List<List<String>> possiblePalindromes) {
+    if (lo == s.length()) {
+      possiblePalindromes.addFirst(new ArrayList<>(current));
       return;
     }
 
-    for (int p = lo; p <= hi; ++p) {
+    for (int p = lo; p < s.length(); ++p) {
       if (isPalindrome(s, lo, p)) {
         current.addLast(s.substring(lo, p + 1));
-        partition(s, p + 1, hi, current, possiblePalindromes);
-
-        if (validList(current, s.length())) {
-          possiblePalindromes.addFirst(new ArrayList<>(current));
-        }
-
+        partition(s, p + 1, current, possiblePalindromes);
         current.removeLast();
       }
     }
   }
 
   boolean isPalindrome(String s, int l, int h) {
-    for (; l <= h; ++l, --h) {
-      if (s.charAt(l) != s.charAt(h)) return false;
+    if (memo[l][h] == null) {
+      for (int i = l, j = h; i <= j; ++i, --j) {
+        if (s.charAt(i) != s.charAt(j)) {
+          return memo[l][h] = false;
+        }
+      }
+      return memo[l][h] = true;
     }
-    return true;
-  }
-
-  boolean validList(List<String> list, int n) {
-    int x = 0;
-    for (String s : list) x += s.length();
-    return x == n;
+    return memo[l][h];
   }
 
   public static void main(String[] args) {
