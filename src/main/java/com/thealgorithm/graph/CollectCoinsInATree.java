cbddf63e26478fc135d5f5f5294b00ddfdd7b699
Subham1999 <subhamsantra2016@gmail.com>
2024-08-31 00:28:10 +0530

    LLD

---
+++ src/main/java/com/thealgorithm/graph/CollectCoinsInATree.java
@@ -0,0 +1,20 @@
+package com.thealgorithm.graph;
+
+/**
+ * @author: Subham Santra
+ */
+public class CollectCoinsInATree {
+
+  public int collectTheCoins(int[] coins, int[][] edges) {
+    return 0;
+  }
+
+  public static void main(String[] args) {
+
+    System.out.println(
+        new CollectCoinsInATree()
+            .collectTheCoins(
+                new int[] {1, 0, 0, 0, 0, 1},
+                new int[][] {{0, 1}, {1, 2}, {2, 3}, {3, 4}, {4, 5}}));
+  }
+}
---
+++ src/main/java/com/thealgorithm/lld/ParkingLotLLD.java
@@ -0,0 +1,111 @@
+package com.thealgorithm.lld;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.List;
+import lombok.Data;
+
+/**
+ * @author: Subham Santra
+ */
+@Data
+abstract class Vehicle {
+  String vehicleNumber;
+  String registration;
+  String owner;
+}
+
+class Bike extends Vehicle {}
+
+class Car extends Vehicle {}
+
+class Truck extends Vehicle {}
+
+@Data
+abstract class ParkingSpot {
+  int level;
+  int id;
+  boolean isOccupied;
+  Vehicle occupiedVehicle;
+  Timestamp occupiedAt;
+
+  abstract boolean canOccupy(Vehicle vehicle);
+
+  public boolean occupy(Vehicle vehicle) {
+    if (canOccupy(vehicle)) {
+      isOccupied = true;
+      occupiedVehicle = vehicle;
+      occupiedAt = Timestamp.from(Instant.now());
+      return true;
+    }
+    return false;
+  }
+}
+
+class BikeParkingSpot extends ParkingSpot {
+  @Override
+  boolean canOccupy(Vehicle vehicle) {
+    return (vehicle instanceof Bike) && !isOccupied();
+  }
+}
+
+class CarParkingSpot extends ParkingSpot {
+  @Override
+  boolean canOccupy(Vehicle vehicle) {
+    return (vehicle instanceof Car) && !isOccupied();
+  }
+}
+
+class TruckParkingSpot extends ParkingSpot {
+  @Override
+  boolean canOccupy(Vehicle vehicle) {
+    return (vehicle instanceof Car) && !isOccupied();
+  }
+}
+
+class ParkingSpotNotEmptyException extends Exception {
+  public ParkingSpotNotEmptyException(int parkingSpotId) {
+    super(Integer.toString(parkingSpotId));
+  }
+}
+
+@Data
+class Level {
+  int level;
+  List<ParkingSpot> parkingSpots;
+
+  void addMore(ParkingSpot parkingSpot) {
+    parkingSpot.setLevel(level);
+    this.parkingSpots.add(parkingSpot);
+  }
+
+  void remove(int parkingSpotId) throws ParkingSpotNotEmptyException {
+    for (ParkingSpot parkingSpot : parkingSpots) {
+      if (parkingSpot.getId() == parkingSpotId) {
+        if (parkingSpot.isOccupied()) {
+          throw new ParkingSpotNotEmptyException(parkingSpotId);
+        } else {
+          parkingSpots.remove(parkingSpot);
+        }
+      }
+    }
+  }
+
+  ParkingSpot parkThis(Vehicle vehicle) {
+    for (ParkingSpot parkingSpot : parkingSpots) {
+      if (parkingSpot.occupy(vehicle)) {
+        return parkingSpot;
+      }
+    }
+    return null;
+  }
+}
+
+@Data
+class ParkingLot {
+  List<Level> parkingLevels;
+
+  void park(Vehicle vehicle) {}
+}
+
+public class ParkingLotLLD {}
